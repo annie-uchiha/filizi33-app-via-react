dc5a2c8b9dc4c6b85377c0dcd91fb10001a44356
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-05-25 21:58:37 +0300

    initial setup

--- filizi33-app/src/App.js
+++ filizi33-app/src/App.js
@@ -1,28 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Landing from "./Landing/landing";
-import Login from "./Login/login";
+import Landing from "./LandingProfilePage/Landing";
+import Login from "./Login/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Application() {
+  const isLoggedIn = true;
+  return <div>{!isLoggedIn ? <Login /> : <landing />}</div>;
 }
+
+export default Application;
+
 
 export default App;
 
