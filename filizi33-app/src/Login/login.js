fa8ca3b7eb51bd185b0ea41bb90902e3908391ce
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-05-26 22:08:23 +0300

    problem with setting props

--- filizi33-app/src/Login/login.js
+++ filizi33-app/src/Login/login.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import { users } from "../users"
+import { users } from "../users";
 import "./Login.scss";
 
 function Login({ onLogin }) {
